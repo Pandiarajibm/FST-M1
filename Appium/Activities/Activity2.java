571622a87c8b7ddadd5d5d63f899f60832442c54
PANDIARAJAN ALAGAPPAKONE <pandia12@in.ibm.com>
2025-03-22 12:11:37 +0530

    Appium Files uploaded

--- Appium/Activities/Activity2.java
+++ Appium/Activities/Activity2.java
@@ -1,61 +1,84 @@
 package activities;
 
+import static org.testng.Assert.assertEquals;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.time.Duration;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+//import io.appium.java_client.ios.IOSDriver;
+//import io.appium.java_client.ios.options.XCUITestOptions;
 
 public class Activity2 {
-    // Driver Declaration
-    AndroidDriver driver;
+	// Driver declaration
+	AppiumDriver androidDriver;
+	AppiumDriver iosDriver;
 
-    // Set up method
-    @BeforeClass
-    public void setUp() throws MalformedURLException, URISyntaxException {
-        // Desired Capabilities
-        UiAutomator2Options options = new UiAutomator2Options();
-        options.setPlatformName("android");
-        options.setAutomationName("UiAutomator2");
-        options.setAppPackage("com.android.chrome");
-        options.setAppActivity("com.google.android.apps.chrome.Main");
-        options.noReset();
+	WebDriverWait wait;
 
-        // Set the Appium server URL
-        URL serverURL = new URI("http://localhost:4723").toURL();
+	// Setup function
+	@BeforeClass
+	public void androidSetUp() throws MalformedURLException, URISyntaxException {
+		// Set desired capabilities
+		UiAutomator2Options options = new UiAutomator2Options().setPlatformName("android")
+				.setAutomationName("UiAutomator2").setAppPackage("com.android.chrome")
+				.setAppActivity("com.google.android.apps.chrome.Main").noReset();
 
-        // Driver Initialization
-        driver = new AndroidDriver(serverURL, options);
+		// Set Appium server url ( http://127.0.0.1:4723/ )
+		URL serverURL = new URI("http://127.0.0.1:4723/").toURL();
 
-        // Open the page in Chrome
-        driver.get("https://training-support.net");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    }
+		// Initialize driver
+		androidDriver = new AndroidDriver(serverURL, options);
 
-    // Test method
-    @Test
-    public void chromeTest() {
-        // Find heading on the page
-        String pageHeading = driver.findElement(AppiumBy.xpath(
-                "//android.widget.TextView[@text='Training Support']"
-        )).getText();
+		wait = new WebDriverWait(androidDriver, Duration.ofSeconds(10));
 
-        // Print to console
-        System.out.println("Heading: " + pageHeading);
+		androidDriver.get("https://training-support.net");
+	}
 
-        // Find and click the About Us link
-        driver.findElement(AppiumBy.accessibilityId("About Us")).click();
+	@Test
+	public void chromeTest() { // Make sure to open the app in the emulator before starting the test else it
+								// will fail
+		// Find page heading
 
-        // Find heading of new page and print to console
-        String aboutPageHeading = driver.findElement(AppiumBy.xpath(
-                "//android.widget.TextView[@text='About Us']"
-        )).getText();
-        System.out.println(aboutPageHeading);
-    }
+		String pageHeading = wait.until(ExpectedConditions
+				.visibilityOfElementLocated(AppiumBy.xpath("//android.widget.TextView[@text=\"Training Support\"]")))
+				.getText();
+		System.out.println(pageHeading);
+		
+		//Find About Us botton, click to go to next page and assert page heading of next page
+	}
 
+	@AfterClass
+	public void tearDown() {
+		// Close the app
+		androidDriver.quit();
+	}
 
-    // Tear down method
-    @AfterClass
-    public void tearDown() {
-        // Close the app
-        driver.quit();
-    }
+//	@BeforeClass
+//	public void iosSetUp() throws MalformedURLException, URISyntaxException {
+//		// Set desired capabilities
+//		XCUITestOptions options = new XCUITestOptions().
+//				setPlatformName("ios").
+//				setAutomationName("XCUITest").
+//				setApp("path/to/file.ipa").
+//				noReset();
+//
+//		// Set Appium server url (  http://127.0.0.1:4723/ )
+//		URL serverURL = new URI("http://127.0.0.1:4723/").toURL();
+//		// Initialize driver
+//		iosDriver = new IOSDriver(serverURL, options);
+//
+//	}
 }
--- Appium/Activities/Activity6.java
+++ Appium/Activities/Activity6.java
@@ -1,76 +1,94 @@
 package activities;
-import static  org.
-	public class Activity6 {
-		AndroidDriver driver;
-		WebDriverWait wait;
+import static org.testng.Assert.assertTrue;
 
-		@BeforeClass
-		public void setUp() throws MalformedURLException, URISyntaxException {
-			// Desired Capabilities
-			UiAutomator2Options options = new UiAutomator2Options();
-			options.setPlatformName("Android");
-			options.setAutomationName("UiAutomator2");
-			options.setAppPackage("com.android.chrome");
-			options.setAppActivity("com.google.android.apps.chrome.Main");
-			options.noReset();
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.time.Duration;
 
-			// Server URL
-			URL serverURL = new URI("http://localhost:4723").toURL();
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-			// Driver initialization
-			driver = new AndroidDriver(serverURL, options);
-			wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
 
-			// Open Selenium page
-			driver.get("https://training-support.net/webelements/sliders");
-		}
+public class Activity6{
+	AndroidDriver driver;
+	WebDriverWait wait;
 
-		@Test
-		public void volume75Test() {
-			// Wait for page to load
-			wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.SeekBar")));
-			// Get the size of the screen
-			Dimension dims = driver.manage().window().getSize();
-			// Set the start and end points
-			Point start = new Point((int) (dims.getWidth() * .50), (int) (dims.getHeight() * .72));
-			Point end = new Point((int) (dims.getWidth() * .665), (int) (dims.getHeight() * .72));
-			// Perform swipe
-			doSwipe(driver, start, end, 2000);
+	@BeforeClass
+	public void setUp() throws MalformedURLException, URISyntaxException {
+		// Desired Capabilities
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setPlatformName("Android");
+		options.setAutomationName("UiAutomator2");
+		options.setAppPackage("com.android.chrome");
+		options.setAppActivity("com.google.android.apps.chrome.Main");
+		options.noReset();
 
-			// Get the volume level
-			String volumeText = driver
-				.findElement(AppiumBy.xpath("//android.view.View/android.widget.TextView[contains(@text, '%')]"))
-				.getText();
+		// Server URL
+		URL serverURL = new URI("http://localhost:4723").toURL();
 
-			// Assertions
-			assertTrue(volumeText.contains("75%"));
-		}
+		// Driver initialization
+		driver = new AndroidDriver(serverURL, options);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-		@Test
-		public void volume25Test() {
-			// Wait for page to load
-			wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.SeekBar")));
-			// Get the size of the screen
-			Dimension dims = driver.manage().window().getSize();
-			// Set the start and end points
-			Point start = new Point((int) (dims.getWidth() * .50), (int) (dims.getHeight() * .72));
-			Point end = new Point((int) (dims.getWidth() * .333), (int) (dims.getHeight() * .77));
-			// Perform swipe
-			doSwipe(driver, start, end, 2000);
+		// Open Selenium page
+		driver.get("https://training-support.net/webelements/sliders");
+	}
 
-			// Get the volume level
-			String volumeText = driver
-				.findElement(AppiumBy.xpath("//android.view.View/android.widget.TextView[contains(@text, '%')]"))
-				.getText();
+	@Test
+	public void volume75Test() {
+		// Wait for page to load
+		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.SeekBar")));
+		// Get the size of the screen
+		Dimension dims = driver.manage().window().getSize();
+		// Set the start and end points
+		Point start = new Point((int) (dims.getWidth() * .50), (int) (dims.getHeight() * .75));
+		Point end = new Point((int) (dims.getWidth() * .666), (int) (dims.getHeight() * .75));
+		// Perform swipe
+		ActionsBase.doSwipe(driver, start, end, 2000);
 
-			// Assertions
-			assertTrue(volumeText.contains("25%"));
-		}
+		// Get the volume level
+		String volumeText = driver
+			.findElement(AppiumBy.xpath("//android.view.View/android.widget.TextView[contains(@text, '%')]"))
+			.getText();
 
-		@AfterClass
-		public void tearDown() {
-			// Close the browser
-			driver.quit();
-		}
+		// Assertions
+		assertTrue(volumeText.contains("75%"));
+	}
+
+	@Test
+	public void volume25Test() {
+		// Wait for page to load
+		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.xpath("//android.widget.SeekBar")));
+		// Get the size of the screen
+		Dimension dims = driver.manage().window().getSize();
+		// Set the start and end points
+		Point start = new Point((int) (dims.getWidth() * .50), (int) (dims.getHeight() * .75));
+		Point end = new Point((int) (dims.getWidth() * .333), (int) (dims.getHeight() * .75));
+		// Perform swipe
+		ActionsBase.doSwipe(driver, start, end, 2000);
+
+		// Get the volume level
+		String volumeText = driver
+			.findElement(AppiumBy.xpath("//android.view.View/android.widget.TextView[contains(@text, '%')]"))
+			.getText();
+
+		// Assertions
+		assertTrue(volumeText.contains("25%"));
+	}
+
+	@AfterClass
+	public void tearDown() {
+		// Close the browser
+		driver.quit();
 	}
 }
